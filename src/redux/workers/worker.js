c55bb547db00e7ffc2a118d2176cad6fb8f07cea
Ivan Holovach <igolovac629@gmail.com>
2020-02-19 18:35:00 +0200

    initial heroku commit

--- src/redux/workers/worker.js
+++ src/redux/workers/worker.js
@@ -8,12 +8,20 @@ const toggleDetection = () => {
     store.isDetection = !store.isDetection;
 };
 
+const YOUR_ACCESS_KEY = '7e6d0160d3e3ccdffb4d3658d3ad9024';
+
 const fetchUpdateUserPosition = async () => {
     if (store.isDetection) {
-        const response = await fetch('http://ip-api.com/json/');
-        const { lat, lon } = await response.json();
+        // const ipAddressResponse = await fetch('https://api.ipify.org/?format=json');
+        // const { ip: ipAddress } = await ipAddressResponse.json();
 
-        postMessage({ lat, lon });
+        const geoLocationsResponse = await fetch(`http://api.ipapi.com/check?access_key=${YOUR_ACCESS_KEY}`, {
+            "method": "GET"
+        });
+
+        const { latitude, longitude } = await geoLocationsResponse.json();
+
+        postMessage({ lat: latitude, lon: longitude });
         setTimeout(() => fetchUpdateUserPosition(), USER_LOCATION_DETECTION_TIMOUT);
     }
 };
